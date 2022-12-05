2e62927dfe95010b212ed669647d049e7276f301
Ankush Kumar <ankush1382003@gmail.com>
2022-12-05 21:50:43 +0530

    Updates

--- main.py
+++ main.py
@@ -7,34 +7,34 @@ cpu_score = 0
 player_score=0
 
 while True:
-    player = input("Choose Rock, Paper or Scissor").capitalize()
+    player = input("Choose Rock, Paper or Scissor: ").capitalize()
     if player == computer:
-        print("You and Computer chose the same. It's a Tie!")
+        print("\tYou and Computer chose the same. It's a Tie!")
     elif player == 'Rock':
         if computer == 'Paper':
-            print("You lose!",computer,"covers",player)
+            print("\tYou lose!",computer,"covers",player)
             cpu_score+=1
         else:
-            print("You win!",player,"smashes",computer)
+            print("\tYou win!",player,"smashes",computer)
             player_score+=1
     elif player == 'Paper':
         if computer == 'Scissor':
-            print ("You lose!",computer,"cuts",player)
+            print ("\tYou lose!",computer,"cuts",player)
             cpu_score+=1
         else:
-            print("You win!",player,"covers",computer)
+            print("\tYou win!",player,"covers",computer)
             player_score+=1
     elif player == 'Scissor':
         if computer == 'Rock':
-            print ("You lose!",computer,"smashes",player)
+            print ("\tYou lose!",computer,"smashes",player)
             cpu_score+=1
         else:
-            print("You win!",player,"cuts",computer)
+            print("\tYou win!",player,"cuts",computer)
             player_score+=1
 
     elif player=='End':
         print("Final Scores:")
-        print(f"CPU:{cpu_score}")
-        print(f"Plaer:{player_score}")
+        print(f"\tComputer:{cpu_score}")
+        print(f"\tPlayer:{player_score}")
         break
 
